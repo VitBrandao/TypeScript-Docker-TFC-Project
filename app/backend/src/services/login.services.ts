654450c36588d26050758e12522ab34bee55dea2
vitor <vitorsbsilva@gmail.com>
2022-06-07 15:21:41 -0300

    feat: validate method in login services

--- app/backend/src/services/login.services.ts
+++ app/backend/src/services/login.services.ts
@@ -1,3 +1,5 @@
+import JWT = require('jsonwebtoken');
+import readJWT from '../auth/readJWT';
 import User from '../database/models/user';
 
 class LoginServices {
@@ -12,6 +14,20 @@ class LoginServices {
       role,
       email,
     };
+  };
+
+  public validate = async (auth: string) => {
+    const verifyJWT = JWT.verify(auth, readJWT) as { data: { email: string } };
+    if (!verifyJWT) throw new Error('Token not valid');
+
+    const { data: { email } } = verifyJWT;
+
+    const findUser = await User.findOne({ where: { email } });
+    if (!findUser) throw new Error('User not found!');
+
+    const { role } = findUser;
+
+    return role;
   };
 }
 
