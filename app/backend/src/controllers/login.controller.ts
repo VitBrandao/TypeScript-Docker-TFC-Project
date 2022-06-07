95a1d773dd02e4f3fdf5ecd28fd84123ba97919e
vitor <vitorsbsilva@gmail.com>
2022-06-07 15:21:16 -0300

    feat: validate method in login controller

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express';
-// import JWT = require('jsonwebtoken');
-// import readJWT from '../auth/readJWT';
 import createJWT from '../auth/createJWT';
 import LoginServices from '../services/login.services';
 
@@ -14,16 +12,18 @@ class LoginController {
       .json({ user: userLogin, token: await createJWT({ email, password }) });
   };
 
-  // public validate = async (req: Request, _res: Response) => {
-  //   const { authorization } = req.headers;
+  public validate = async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
 
-  //   try {
-  //     const verifyJWT = JWT.verify(authorization as string, readJWT);
-
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+    if (!authorization) throw new Error('Token not found');
+    try {
+      const verify = await this.loginServices.validate(authorization);
+      // const { role } = verify;
+      return res.status(200).json(verify);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
 
 export default LoginController;
