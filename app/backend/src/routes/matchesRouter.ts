a992e33e8c3a48d5d524315cfc5a1d86ec9d484f
vitor <vitorsbsilva@gmail.com>
2022-06-08 16:40:03 -0300

    fix: matches router getAll to get

--- app/backend/src/routes/matchesRouter.ts
+++ app/backend/src/routes/matchesRouter.ts
@@ -4,6 +4,6 @@ import MatchesController from '../controllers/matches.controller';
 const matchesRouter = Router();
 const controller = new MatchesController();
 
-matchesRouter.get('/', controller.getAll);
+matchesRouter.get('/', controller.get);
 
 export default matchesRouter;
