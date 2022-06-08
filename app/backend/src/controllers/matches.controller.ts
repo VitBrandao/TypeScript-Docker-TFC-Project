7c8f81bc72365cb5a321d6b7976e90414bb0ac9b
vitor <vitorsbsilva@gmail.com>
2022-06-08 16:41:00 -0300

    feat: function getAll changed to cover the inProgress received in req.query

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -4,8 +4,18 @@ import MatchesServices from '../services/matches.services';
 class MatchesController {
   constructor(private matchesServices = new MatchesServices()) { }
 
-  public getAll = async (_req: Request, res: Response) => {
-    const matches = await this.matchesServices.getAll();
+  public get = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+
+    // getAll
+    if (!inProgress) {
+      const matches = await this.matchesServices.getAll();
+      return res.status(200).json(matches);
+    }
+
+    // getByProgress
+    if (typeof inProgress !== 'string') throw new Error('Progress is not a string');
+    const matches = await this.matchesServices.getByProgress(inProgress);
     return res.status(200).json(matches);
   };
 }
