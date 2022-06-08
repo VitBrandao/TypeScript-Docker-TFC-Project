653e36c70db5e452b12cee41583b8e8621173699
vitor <vitorsbsilva@gmail.com>
2022-06-08 16:41:19 -0300

    feat: method getByProgress in matches services

--- app/backend/src/services/matches.services.ts
+++ app/backend/src/services/matches.services.ts
@@ -11,6 +11,20 @@ class MatchesServices {
     });
     return findMatches;
   };
+
+  public getByProgress = async (status: string) => {
+    const progressStatus = status === 'true';
+
+    const findMatches = await Match.findAll({
+      where: { inProgress: progressStatus },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+
+    return findMatches;
+  };
 }
 
 export default MatchesServices;
