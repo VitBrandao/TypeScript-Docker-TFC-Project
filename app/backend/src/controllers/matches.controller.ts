18852ceb9a04dbbed5e8f76825951837a8caa06a
vitor <vitorsbsilva@gmail.com>
2022-06-09 11:32:42 -0300

    feat: refactor matches controller returns to cover id validations

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -23,7 +23,19 @@ class MatchesController {
     const { authorization } = req.headers;
     if (!authorization) throw new Error('Token not found');
 
+    const { homeTeam, awayTeam } = req.body;
+    if (homeTeam === awayTeam) {
+      return res.status(401)
+        .json({ message: 'It is not possible to create a match with two equal teams' });
+    }
+
     const createMatch = await this.matchesServices.create(req.body, authorization);
+
+    if (typeof createMatch === 'string') {
+      return res.status(404)
+        .json({ message: 'There is no team with such id!' });
+    }
+
     return res.status(201).json(createMatch);
   };
 
