002cf615b4e877f6dc829d7e477dd22272944249
vitor <vitorsbsilva@gmail.com>
2022-06-14 11:50:30 -0300

    feat: create TeamData helper

---
+++ app/backend/src/helpers/TeamData.ts
@@ -0,0 +1,11 @@
+class TeamData {
+  id: number;
+  teamName: string;
+
+  constructor(id: number, teamName: string) {
+    this.id = id;
+    this.teamName = teamName;
+  }
+}
+
+export default TeamData;
