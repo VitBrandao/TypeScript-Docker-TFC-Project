7c448ef86e4bd224205d97fa9a536ec834737d22
vitor <vitorsbsilva@gmail.com>
2022-06-14 11:49:56 -0300

    feat: leaderboard router

---
+++ app/backend/src/routes/leaderboardRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import LeaderboardController from '../controllers/leaderboard.controller';
+
+const leaderboardRouter = Router();
+const controller = new LeaderboardController();
+
+leaderboardRouter.get('/', controller.leaderboard);
+// leaderboardRouter.get('/home', controller.homeLeaderboard);
+// leaderboardRouter.get('/away', controller.awayLeaderboard);
+
+export default leaderboardRouter;
