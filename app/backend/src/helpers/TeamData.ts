925dfc6ff7acd5bbfee3a635f40ea0b067de3652
vitor <vitorsbsilva@gmail.com>
2022-06-14 12:08:54 -0300

    add: addNewGame methos in TeamData

--- app/backend/src/helpers/TeamData.ts
+++ app/backend/src/helpers/TeamData.ts
@@ -6,6 +6,44 @@ class TeamData {
     this.id = id;
     this.teamName = teamName;
   }
+
+  // Todos os campos do leaderboard iniciam zerados
+  totalPoints = 0;
+  totalGames = 0;
+  totalVictories = 0;
+  totalDraws = 0;
+  totalLosses = 0;
+  goalsFavor = 0;
+  goalsOwn = 0;
+  goalsBalance = 0;
+  efficiency = 0;
+
+  public calcPointsAndEfficiency = () => {
+    // Pontos
+    this.totalPoints = this.totalDraws + (this.totalVictories * 3);
+
+    // %
+    // eslint-disable-next-line no-mixed-operators
+    this.efficiency = this.totalPoints / (this.totalGames * 3) * 100;
+  };
+
+  public addNewGame = (goalsPro: number, goalsOwn: number) => {
+    // Gols pró, gols contra e saldo de gols
+    this.goalsFavor += goalsPro;
+    this.goalsOwn += goalsOwn;
+    this.goalsBalance = this.goalsFavor - this.goalsBalance;
+
+    // Partidas jogadas
+    this.totalGames += 1;
+
+    // V, E e D
+    if (goalsPro > goalsOwn) this.totalVictories += 1;
+    if (goalsPro < goalsOwn) this.totalLosses += 1;
+    if (goalsPro === goalsOwn) this.totalDraws += 1;
+
+    // Lint - pontos e % de aproveitamento calculados em nova função
+    this.calcPointsAndEfficiency();
+  };
 }
 
 export default TeamData;
